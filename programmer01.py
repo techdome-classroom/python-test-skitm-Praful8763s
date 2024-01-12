eb7e5d3189531b6d21e18b05d4104687167bd003
Praful Sonwane <prafulsonwane13@gmail.com>
2024-01-12 14:27:35 +0530

    submit

---
+++ programmer01.py
@@ -0,0 +1,18 @@
+def isValid(s):
+    stack = []
+    bracket_pairs = {')': '(', '}': '{', ']': '['}
+
+    for char in s:
+        if char in bracket_pairs.values():
+            stack.append(char)
+        elif char in bracket_pairs.keys():
+            if not stack or bracket_pairs[char] != stack.pop():
+                return False
+        else:
+            return False
+
+    return not stack
+
+print(isValid("()"))         
+print(isValid("()[]{}"))     
+print(isValid("(]"))          
